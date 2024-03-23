d3bce6342916251847af9a9505c23dbd4604715a
tom.spyropoulos <tomody.spy@gmail.com>
2024-03-23 18:36:05 +0200

    add dummy functions

--- src/logic.py
+++ src/logic.py
@@ -1,6 +1,21 @@
 
+def bootstrap():
+    pass
+
 def broadcast_block():
     pass
 
 def broadcast_transaction():
+    pass
+
+def create_block():
+    pass
+
+def create_transaction():
+    pass
+
+def receive_block():
+    pass
+
+def receive_transaction():
     pass
