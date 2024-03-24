82f23eb27b0ef750ae78875704cfd9b2eacf5e3c
nickbrakis <nibrakis@gmail.com>
2024-03-24 21:08:58 +0200

    basic client/api

--- src/api.py
+++ src/api.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 import json
 import argparse
@@ -8,23 +9,57 @@ from blockchain import Blockchain
 from transaction import Transaction
 from logic import Node
 
-
 app = FastAPI()
 
 # initialize a blockchain and a node
 blockchain = Blockchain()
 node = Node()
 
+
 ############### client/api ######################
 
-@app.post("/transactions/new")
-async def create_transaction(receiver_address : str, amount : int):
-    
+@app.post("/create_transactions/{receiver_id}/{amount}")
+async def create_transaction(receiver_address: str, amount: int):
     # TO DO : 
     # maybe some checks??
 
-
-    # 1. create a tranasaction
+    # 1. create a transaction and add to pending transactions
     # maybe we change logic to node 
-    transaction = node.create_transaction()
-    # 2. 
+    transaction = node.create_transaction(receiver_address, amount)
+    # 2. broadcast the transaction 
+    node.broadcast_transaction(transaction)
+
+    return JSONResponse("Transaction created and broadcasted successfully", status_code=status.HTTP_200_OK)
+
+@app.get("/view_last_block")
+async def view_last_block():
+    # view block function of backend returns last block
+    last_block = node.view_block()
+    transactions_list = last_block.transactions
+    validator_id = last_block.validator
+    
+    transactions = []
+    for transaction in transactions_list:
+        transactions.append(
+            {
+                "sender_id": node.ring[transaction.sender_address]['id'],
+                # "sender_address": transaction.sender_address,
+                "receiver_id": node.ring[transaction.receiver_address]['id'],
+                # "receiver_address": transaction.receiver_address,
+                "amount": transaction.amount
+            }
+        )
+    transactions.append({"validator" : validator_id})
+
+    return JSONResponse(transactions, status_code=status.HTTP_200_OK)
+
+@app.get("/balance")
+async def get_balance():
+    balance = node.get_balance()
+    return JSONResponse({"balance": balance}, status_code=status.HTTP_200_OK)
+
+
+@app.post("/set_stake/{amount}")
+async def set_stake(amount):
+    node.set_stake(amount)
+    return JSONResponse(f"Stake set to {amount}", status_code=status.HTTP_200_OK)
--- src/logic.py
+++ src/logic.py
@@ -1,21 +1,32 @@
+class Node : 
+    def bootstrap():
+        pass
 
-def bootstrap():
-    pass
+    def broadcast_block():
+        pass
 
-def broadcast_block():
-    pass
+    def broadcast_transaction():
+        pass
 
-def broadcast_transaction():
-    pass
+    def create_block():
+        pass
 
-def create_block():
-    pass
+    def create_transaction():
+        pass
 
-def create_transaction():
-    pass
+    def receive_block():
+        pass
 
-def receive_block():
-    pass
+    def receive_transaction():
+        pass
 
-def receive_transaction():
-    pass
+
+    # new functions added 
+    def view_block():
+        pass
+    
+    def get_balance():
+        pass
+
+    def set_stake(amount: int):
+        pass
